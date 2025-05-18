8c007d4631c10601d93d20d6cc9687b68c70be2f
mohd_hassan <mohhddhassan@gmail.com>
2025-05-18 21:36:36 +0530

    Update projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -10,7 +10,7 @@ export const projectsData = [
     },
     {
         id: 2,
-        name: 'AutoTrend – Demand Prediction System',
+        name: 'AutoTrend - Demand Prediction System',
         description: 'Developed a system using AutoML for end-to-end demand prediction, including preprocessing, EDA, model training, and evaluation. Enabled users to upload datasets, visualize EDA, select target columns, and view top 5 model results. Built ensemble model from top models for final predictions and performance comparison.',
         tools: ['Python', 'Streamlit', 'Scikit-learn', 'Pandas', 'YData Profiling'],
         role: 'Final Year Project | Team of 2',
@@ -19,7 +19,7 @@ export const projectsData = [
     },
     {
         id: 3,
-        name: 'MedRem – Medicine Reminder Web App',
+        name: 'MedRem - Medicine Reminder Web App',
         description: 'Developed a web app that lets users schedule, track, and get reminders for their medications. Designed simple UI for adding meds, setting times, and tracking daily intake. Led the team through problem solving and backend integration during the 24-hour hackathon. Focused on building a functional MVP within the tight deadline.',
         tools: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'PHP', 'MySQL'],
         role: 'Hackathon Project (TechQuest) | Team of 3',
