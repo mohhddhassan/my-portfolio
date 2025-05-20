7c0c0db3a4e6f9d0bfdb0f4f927e6287825027ef
mohd_hassan <mohhddhassan@gmail.com>
2025-05-20 19:19:11 +0530

    profile icon

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,8 +15,24 @@ export const metadata = {
   description:
     "I'm Mohamed Hussain — a data engineer intern and self-taught developer who enjoys solving real-world problems with clean code and smart systems. Whether it's building real-time data pipelines, crafting intuitive web apps, or competing in hackathons, I'm always driven by curiosity and impact. Let's build things that matter. 🚀",
   icons: {
-    icon: "favicon.ico",
-  }
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", type: "image/x-icon" },
+    ],
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon.ico",
+    other: [
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-192x192.png",
+      },
+      {
+        rel: "android-chrome",
+        url: "/android-chrome-512x512.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
