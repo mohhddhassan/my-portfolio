50e309f52ba1467cc363cc1dc0f35b4102a0c7ff
mohd_hassan <mohhddhassan@gmail.com>
2025-05-20 11:17:41 +0530

    Update profile

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Mohamed Hussain S - Associate Data Engineer",
+  title: "Mohamed Hussain | Data Engineer",
   description:
     "I'm Mohamed Hussain — a data engineer intern and self-taught developer who enjoys solving real-world problems with clean code and smart systems. Whether it's building real-time data pipelines, crafting intuitive web apps, or competing in hackathons, I'm always driven by curiosity and impact. Let's build things that matter. 🚀"
 };
