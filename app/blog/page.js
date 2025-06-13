e215aad2b048f9912119d0715d9e019db9a5c671
mohd_hassan <mohhddhassan@gmail.com>
2025-06-13 19:52:48 +0530

    Update blog-page

--- app/blog/page.js
+++ app/blog/page.js
@@ -4,8 +4,9 @@ import { personalData } from "@/utils/data/personal-data";
 import BlogCard from "../components/homepage/blog/blog-card";
 
 async function getBlogs() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-  next: { revalidate: 60 }
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`, {
+    next: { revalidate: 60 }
+  });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
@@ -21,7 +22,7 @@ async function page() {
   return (
     <div className="py-8">
       <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
+        <div className="flex items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
             All Blog
