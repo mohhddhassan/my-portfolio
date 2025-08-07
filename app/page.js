36c26c41e16da0550f04178aa2b82dbe7b5fd158
Mohamed Hussain S <mohhddhassan@gmail.com>
2025-08-07 12:32:17 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import BlogCard from "./components/homepage/blog/blog-card";
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`, {
@@ -50,7 +51,7 @@ export default async function Home() {
         <Skills />
         <Projects />
         <Education />
-        <Blog blogs={blogs} />
+        <Blog blogs={BlogCard} />
         <ContactSection />
       </main>
     </div>
