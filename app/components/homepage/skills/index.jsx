fb5451879167cdcdb734c784c5bb1185ed97bf17
mohd_hassan <mohhddhassan@gmail.com>
2026-03-22 08:53:23 +0530

    Update index

--- app/components/homepage/skills/index.jsx
+++ app/components/homepage/skills/index.jsx
@@ -47,7 +47,7 @@ function Skills() {
                   </div>
                 </div>
                 <div className="flex flex-col items-center justify-center gap-3 p-6">
-                  <div className="h-12 sm:h-14">
+                  <div className="h-8 sm:h-10">
                     <Image
                       src={`/images/skills/${skill.toLowerCase().replace(/\s+/g, '')}.svg`}
                       alt={skill}
