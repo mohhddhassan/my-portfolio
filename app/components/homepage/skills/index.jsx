c4687cc0c7a1da4691c2c0b0ade335545dc70845
mohd_hassan <mohhddhassan@gmail.com>
2026-03-22 08:55:23 +0530

    Update index

--- app/components/homepage/skills/index.jsx
+++ app/components/homepage/skills/index.jsx
@@ -53,7 +53,7 @@ function Skills() {
                       alt={skill}
                       width={40}
                       height={40}
-                      className="h-full w-auto rounded-lg"
+                      className="h-full w-full object-contain"
                     />
                   </div>
                   <p className="text-white text-sm sm:text-lg">{skill}</p>
